3047009b697c82a8226167555b060cea39625e7e
Platonov <platonov@MacBook-Pro-Platonov.local>
2023-03-23 22:31:35 +0300

    2 chasa naprasno2

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-
+from http import HTTPStatus
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!', HTTPStatus.OK)
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница', HTTPStatus.OK)
